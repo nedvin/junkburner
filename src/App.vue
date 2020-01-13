46e896d82bcc73769bf7d6d920e4c4c13f991977
nedvin <in.nivde@gmail.com>
2020-01-13 07:23:56 +0100

    styleat homescreen lite

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,26 @@
 <template>
-  <div id="app" class="container wrapper">
-        <header-view/>
-        <router-view id="container-main"/>
-        <footer-view/>
-  </div>
+    <div id="app" class="container wrapper">
+        <header-view />
+        <router-view id="container-main" />
+        <footer-view />
+    </div>
 </template>
 
 <script>
 import footer from "./components/Footer.vue";
 import header from "./components/Header.vue";
 export default {
-  name: 'App',
-  components:
-  {
-    "header-view" : header,
-    "footer-view" : footer
-  }
-}
+    name: "App",
+    components: {
+        "header-view": header,
+        "footer-view": footer
+    }
+};
 </script>
 
 <style>
-
-html, body {
+html,
+body {
     height: 100%;
     width: 100%;
     padding: 0;
@@ -53,22 +52,46 @@ body {
     margin-bottom: 25px;
 }
 
-button {
+.old-button {
     background-color: coral;
     padding: 10px;
     font-weight: bold;
     border: 1px solid Black;
 }
 
+.btn {
+    border: none;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+}
+
+.btn-green {
+    background-color: #05c46b;
+}
+
+.btn-orange {
+    background-color: #ffa801;
+}
+
+.btn-abort {
+    background-color: #ff3f34;
+}
+
 .wrapper {
     padding-bottom: 60px;
-    min-height : calc(100vh - 60px);
+    min-height: calc(100vh - 60px);
 }
 
 @media screen and (max-width: 670px) {
     #container-main {
         flex-direction: column;
     }
-}
 
+    button {
+        padding: 8px 16px;
+    }
+}
 </style>
