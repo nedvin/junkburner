2e6a797174531d7c4191072f9760a1cfb6185967
RillMeister <riwa@kth.se>
2020-01-13 23:27:56 +0100

    User-modell preliminärt klar

--- src/data/modules/User.js
+++ src/data/modules/User.js
@@ -1,24 +1,78 @@
 /**************  STATE ***************************/
 
 const state = {
-
+    age : 0,
+    length : 0,
+    weight : 0,
+    kcalRdi : 0,
+    userName : "",
+    gender : ""
 };
 
 /**************  GETTERS ***************************/
 const getters = {
-
+    age : state => state.age,
+    length : state => state.length,
+    weight : state => state.weight,
+    kcalRdi : state => state.kcalRdi,
+    userName : state => state.userName
 };
 
 /**************  ACTIONS ***************************/
 
 const actions = {
-
+    loadUser({commit}, user){
+        commit("loadUser", user);
+        commit("calculateKcalRdi");
+    },
+    setAge({commit}, age){
+        commit("setAge", age);
+        commit("calculateKcalRdi");
+    },
+    setLength({commit}, length){
+        commit("setLength", length);
+        commit("calculateKcalRdi");
+    },
+    setWeight({commit}, weight){
+        commit("setWeight", weight);
+        commit("calculateKcalRdi");
+    },
+    setGender({commit}, gender){
+        commit("setGender", gender);
+        commit("calculateKcalRdi");
+    }
 
 };
 
 /**************  MUTATIONS ***************************/
 const mutations = {
-
+    loadUser(state, user){
+        state.age = user.age;
+        state.length = user.length;
+        state.weight = user.weight;
+        state.userName = user.userName;
+        state.gender = user.gender;
+    },
+    setAge(state, age){
+        state.age = age;
+    },
+    setLength(state, length){
+        state.length = length;
+    },
+    setWeight(state, weight){
+        state.weight = weight;
+    },
+    setGender(state, gender){
+        state.gender = gender;
+    },
+    calculateKcalRdi(state){ // Revised Harris-Benedict Equation
+        if(state.gender = "F"){
+            state.kcalRdi = Math.round(88.362 + 13.397*state.weight + 4.799*state.length - 5.677*state.age);
+        }
+        else{
+            state.kcalRdi = Math.round(447.593 + 9.247*state.weight + 3.098*state.length - 4.33*state.age);
+        }
+    }
 };
 
 export default {
